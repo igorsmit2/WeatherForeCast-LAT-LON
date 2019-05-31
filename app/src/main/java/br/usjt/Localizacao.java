ee1825c07fdbd10ec418f7c46d83856cc8beefe2
imedeiros <medeiros.igormota@gmail.com>
2019-05-31 16:11:18 -0300

    Aula 06 - SQLite

---
+++ app/src/main/java/br/usjt/Localizacao.java
@@ -0,0 +1,11 @@
+package br.usjt;
+
+public class Localizacao {
+    double latitude;
+    double longitude;
+
+    public Localizacao (double latitude, double longitude){
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}
--- app/src/main/java/br/usjt/MainActivity.java
+++ app/src/main/java/br/usjt/MainActivity.java
@@ -2,39 +2,31 @@ package br.usjt;
 
 import android.Manifest;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private List<Localizacao> localizacoes;
     private MyAdapter           adapter;
-
+    Context context;
     // API
     private LocationListener    locationListener;
     private LocationManager     locationManager;
@@ -60,6 +52,11 @@ public class MainActivity extends AppCompatActivity {
                 double lat = location.getLatitude();
                 double lon = location.getLongitude();
                 Localizacao l = new Localizacao(lat, lon);
+
+                LocationDAO dao = new LocationDAO(context);
+                String sql = dao.insertLocation(localizacoes);
+                GPSDBHelper db = new GPSDBHelper(context);
+                db.insertLocation(sql);
                 if(localizacoes.size() >= 50){
                     localizacoes.remove(0);
                     localizacoes.add(l);
@@ -131,15 +128,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private class Localizacao{
-        double latitude;
-        double longitude;
-
-        public Localizacao (double latitude, double longitude){
-            this.latitude = latitude;
-            this.longitude = longitude;
-        }
-    }
 
     // Ciclo de Vida
 
@@ -158,8 +146,8 @@ public class MainActivity extends AppCompatActivity {
         else{
             locationManager.requestLocationUpdates(
                     LocationManager.GPS_PROVIDER,
-                    0,
-                    0,
+                    1,
+                    1,
                     locationListener
             );
         }
